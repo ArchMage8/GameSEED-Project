83127dedc3a8ef415dd05f8b7a328b6467045dbb
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-09-11 17:37:34 +0700

    rename pt2

--- Assets/Scripts/Player/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnimationHandler : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator animator;
