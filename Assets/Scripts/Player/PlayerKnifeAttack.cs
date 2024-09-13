f55c0837c201359f9731e51d2492525e77c9f384
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-09-13 10:31:36 +0700

    ASKLJDHLKJWQHELKSJDA

--- Assets/Scripts/Player/PlayerKnifeAttack.cs
+++ Assets/Scripts/Player/PlayerKnifeAttack.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class PlayerKnifeAttack : MonoBehaviour
 {
     [Header("Attack Settings")]
-    //public float rayLength = 5f;
     public int damageDealt = 1;
     public float generationDelay = 0.5f;
 
@@ -43,27 +42,22 @@ public class PlayerKnifeAttack : MonoBehaviour
         playerAnimator.SetTrigger("Attack");
 
         playerMovement.canMove = false;
-
         playerMovement.StopMovement();
 
         Throw();
 
         yield return new WaitForSeconds(0.5f);
         if (playerMovement.isGrounded)
-
         {
-
             isAttacking = false;
             canAttack = true;
             playerMovement.canMove = true;
         }
-
         else
         {
             rb.gravityScale = 1;
             StartCoroutine(MidAirHandler());
         }
-
     }
 
     private void Throw()
@@ -73,9 +67,15 @@ public class PlayerKnifeAttack : MonoBehaviour
 
     private IEnumerator GenerateProjectile()
     {
+        // Instantiate the projectile
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
 
-        Vector2 direction = MainObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+        // Get the player's X scale and apply it to the projectile's scale (for visual purposes)
+        Vector3 playerScale = MainObject.transform.localScale;
+        projectile.transform.localScale = new Vector3(playerScale.x, projectile.transform.localScale.y, projectile.transform.localScale.z);
+
+        // Set projectile velocity
+        Vector2 direction = playerScale.x > 0 ? Vector2.right : Vector2.left;
         Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
         projectileRb.velocity = direction * projectileSpeed;
 
