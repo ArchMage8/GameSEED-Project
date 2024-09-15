e73a19a971307fffa7ba16a0ddb4ca8a8b4844e4
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-09-15 23:07:33 +0700

    bossBGM

--- Assets/Scripts/Hostiles/Boss/AttackSystems/PlayerDetector.cs
+++ Assets/Scripts/Hostiles/Boss/AttackSystems/PlayerDetector.cs
@@ -7,6 +7,24 @@ public class PlayerDetector : MonoBehaviour
     [Header("Detection")]
     public bool playerInRange = false;
 
+    [Header("Audio Objects")]
+    public GameObject OriginalBGM;
+    public GameObject BossBGM;
+
+    private void Update()
+    {
+        if (playerInRange == false)
+        {
+            OriginalBGM.SetActive(true);
+            BossBGM.SetActive(false);
+        }
+        else
+        {
+            OriginalBGM.SetActive(false);
+            BossBGM.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
